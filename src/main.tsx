7d489b18e8aeec3a403e72ce86195c82c0fd1382
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2024-08-31 15:32:25 -0300

    Commit Inicial

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.tsx'
+import Login from './routes/Login/index.tsx'
+import Register from './routes/Register/index.tsx'
+import SobreNos from './routes/SobreNos/index.tsx'
+import Error from './routes/Error/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/sobreNos',
+        element: <SobreNos/>
+      },
+      {
+        path: '/login',
+        element: <Login/>
+      },
+      {
+        path: '/register',
+        element: <Register/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
