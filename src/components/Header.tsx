46f6cf8f2bc3fba244f3fc755d0de99b5f7918c7
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2024-08-31 16:00:52 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
+import styles from "../styles/Header.module.css"
+import "../styles/global.module.css"
+
+import logo from "../assets/logo.svg"
 
 export default function Header(){
     return(
-        <nav>
-            <Link to='/'>Home</Link>
-            <span> | </span>
-            <Link to='/produtos'>Produtos</Link>
-        </nav>
+        <header className={styles.cabecalho}>
+            <figure>
+                <img src={logo} alt=""/>
+            </figure>
+            <nav>
+                <ul>
+                    <a href=""><Link to='/'>Home</Link></a>
+                    <a href=""><Link to='/SobreNos'>Sobre Nós</Link></a>
+                    <a href=""><Link to='/#ajuda'>Ajuda</Link></a>
+                </ul>
+            </nav>
+            <div>
+                <button className={`${styles.registrar} ${styles.botoes}`}><a href=""><Link to='/register'>Registrar</Link></a></button>
+                <button className={`${styles.login} ${styles.botoes}`}><a href=""><Link to='/login'>Login</Link></a></button>
+            </div>
+        </header>
     )
 }
