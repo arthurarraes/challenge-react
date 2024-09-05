1898825a437d2e1b7b11d5082bb5635753224f6f
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2024-09-05 08:40:24 -0300

    Update index.tsx

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -5,13 +5,19 @@ import ProblemasComuns from "../../components/ProblemasComuns"
 import DuvidasFrequentes from "../../components/DuvidasFrequentes"
 
 export default function Home(){
+    const handleChatButtonClick = () => {
+        const chatInstance = (window as any).watsonChatInstance;
+        if (chatInstance) {
+          chatInstance.openWindow();
+        }
+      };
     return(
         <main>
             <section className={styles.banner}>
                 <section className={styles.texto}>
                     <header>Descubra em poucos passos, o problema do seu carro.</header>
                     <p>Com a nova IA você descobrirá em qualquer lugar, o defeito do seu veículo.</p>
-                    <button className={`${styles.consulte} ${styles.botoes}`}>Consulte Agora</button>
+                    <button className={`${styles.consulte} ${styles.botoes}`}onClick={handleChatButtonClick}>Consulte Agora</button>
                 </section>
                 <figure>
                     <img src={banner} alt=""/>
